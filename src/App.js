aeb1cdd5f9f3e3e8d95d23ea3650448a6c14be05
c-kehler <charlespkehler@gmail.com>
2024-07-08 18:53:15 -0400

    add py padding

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import FlavorForm from "./FlavorForm";
 
 function App() {
-  return (
-      <FlavorForm />
-  );
+    return (
+        <div className="py-72">
+            <FlavorForm/>
+        </div>
+    );
 }
 
 export default App;
