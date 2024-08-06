d795457ba9da7b3a4ffeab0487a70c39bdc78f02
Aaron White <aaronlacywhite387@gmail.com>
2024-08-06 10:43:13 -0700

    Revert "feat: chat widget"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
+import './App.css';
 import FlavorForm from "./FlavorForm";
 
 function App() {
-  return (
-    <div>
-      <img src="/bs-topper-2.jpg" alt="bake-info" />
-      <FlavorForm />
-    </div>
-  );
+    return (
+        <div>
+            <img src="/bs-topper-2.jpg" alt="bake-info"/>
+            <FlavorForm/>
+        </div>
+    );
 }
 
 export default App;
