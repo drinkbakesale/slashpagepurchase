57582f0d8b4a05cf07344db41498f8bba3d248d3
drinkbakesale <will@drinkbakesale.com>
2024-09-05 07:38:01 -0600

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -27,7 +27,7 @@ const FlavorForm = () => {
     "Chocolate Chip Cookie": "43724077891874",
     Brownie: "46649579372834",
     "Thin Mint": "47594913628450",
-    "Limited Edition 5-Layer Bar": "49097034596642",
+    "Limited Edition 5-Layer Bar": "49077761802530",
   };
 
   function buildShopifyCartUrl(cartItems) {
