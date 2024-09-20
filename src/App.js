c00f085b6994bb0b2536943535fbd0bcf8372878
aalacy <aaronlacywhite387@gmail.com>
2024-09-20 11:39:00 -0700

    fix: issue

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ function App() {
     <div className="overflow-y-auto h-[100vh] pb-3">
       <img src="/bs-topper-2.jpg" alt="bake-info" />
       <FlavorForm />
+      <div
+        style={{ margin: "-0.3rem 0.5rem", position: "relative", zIndex: 100 }}
+      >
+        <div
+          id="chatImage"
+          style={{
+            cursor: "pointer",
+            background: "#ddbcbd",
+            border: "2px solid #7c0101",
+            padding: "1rem",
+            fontWeight: "bold",
+            textAlign: "center",
+            color: "#7c0101",
+            borderRadius: "0.5rem",
+          }}
+        >
+          Have questions? Click for live chat!
+        </div>
+      </div>
     </div>
   );
 }
