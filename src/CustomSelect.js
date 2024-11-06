7e6048f1500ec206dc867236caf792dda5048908
drinkbakesale <will@drinkbakesale.com>
2024-11-06 10:17:14 -0700

    Update CustomSelect.js

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -12,7 +12,7 @@ const flavorOptions = [
     {
         value: 'Limited Edition Cinnamon Roll',
         label: 'Limited Edition Cinnamon Roll',
-        subText: 'Rich cinnamon notes swirl through buttery dough and sweet icing',
+        subText: 'Cinnamon swirled buttery dough with sweet icing',
         color: '#A25D33',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cinnamon_Roll-min.png?v=1730832932',
         textColor: '#FFFFFF',
