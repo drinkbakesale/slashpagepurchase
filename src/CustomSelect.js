4140cda8a4d12b42f9c279af0bff3107a773aaa6
drinkbakesale <will@drinkbakesale.com>
2024-11-22 12:57:31 -0700

    Update CustomSelect.js

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -1,6 +1,69 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const flavorOptions = [
+        {
+        value: 'Limited Edition 5-Layer Bar',
+        label: 'Limited Edition 5-Layer Bar',
+        subText: 'A blondie with layers of toasted coconut and caramel',
+        color: '#C7832E',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/5-layer_Bar_-_Tiny.png?v=1725552129',
+        textColor: '#FFFFFF',
+        product_id: '9456474456354',
+    },
+         {
+        value: 'Limited Edition Banana Bread',
+        label: 'Limited Edition Banana Bread',
+        subText: 'Ripe bananas, vanilla, and cinnamon in cake-like harmony',
+        color: '#FBE04B',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_Banana_Bread-min_a54d6154-5221-4c95-9356-d579b4f20e87.png?v=1727897778',
+        textColor: '#830005',
+        product_id: '9493909438754',
+    },
+      {
+        value: 'Limited Edition Blueberry Pie',
+        label: 'Limited Edition Blueberry Pie',
+        subText: 'Sun-kissed blueberries meet buttery, flaky pie crust',
+        color: '#4E70B6',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/tiny-blueberry_pie.png?v=1722450830',
+        textColor: '#FFFFFF',
+        product_id: '9395140460834',
+    },
+      {
+        value: 'Limited Edition Lemon Bar',
+        label: 'Limited Edition Lemon Bar',
+        subText: 'Zesty lemon filling over rich, honeyed graham cracker',
+        color: '#FFC627',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Lemon_Bar-min.png?v=1732037885',
+        textColor: '#7C0101',
+        product_id: '9016836620578',
+    },
+      {
+        value: 'Limited Edition Peach Cobbler',
+        label: 'Limited Edition Peach Cobbler',
+        subText: 'Juicy peaches, cinnamon, and brown sugar—summer in a sip',
+        color: '#F99D22',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Peach_Cobbler.png?v=1719851632',
+        textColor: '#FFFFFF',
+        product_id: '9344663159074',
+    },
+      {
+        value: 'Limited Edition Smores',
+        label: 'Limited Edition Smores',
+        subText: 'Toasted marshmallow, rich chocolate, and graham cracker',
+        color: '#4B2D13',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Smores.png?v=1718393168',
+        textColor: '#FFFFFF',
+        product_id: '9260360007970',
+    },
+      {
+        value: 'Limited Edition Gingerbread',
+        label: 'Limited Edition Gingerbread',
+        subText: 'Spiced gingerbread cookie with sweet molasses and warm cinnamon',
+        color: '#3EB65F',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Gingerbread-min.png?v=1732037885',
+        textColor: '#FFFFFF',
+        product_id: '9260360007970',
+    },
     {
         value: 'Variety Pack #1',
         label: 'Variety Pack #1',
@@ -8,6 +71,7 @@ const flavorOptions = [
         color: '#87c8d5',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Flavor_Adventure_w_Shadow.png?v=1718393167',
         textColor: '#7C0101',
+        product_id: '9342358421794',
     },
     {
         value: 'Limited Edition Cinnamon Roll',
@@ -16,6 +80,7 @@ const flavorOptions = [
         color: '#A25D33',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cinnamon_Roll-min.png?v=1730832932',
         textColor: '#FFFFFF',
+        product_id: '9525727002914',
     },
     {
         value: 'Jelly Donut',
@@ -24,6 +89,7 @@ const flavorOptions = [
         color: '#e27b9c',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Jelly_Donut.png?v=1718393168',
         textColor: '#7C0101',
+        product_id: '9342359044386',
     },
     {
         value: 'Chocolate Chip Cookie',
@@ -32,6 +98,7 @@ const flavorOptions = [
         color: '#e88b37',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cookie_Box.png?v=1718393167',
         textColor: '#7C0101',
+        product_id: '9342357668130',
     },
     {
         value: 'Brownie',
@@ -40,6 +107,7 @@ const flavorOptions = [
         color: '#9965a2',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Brownie_Box.png?v=1718393167',
         textColor: '#FFFFFF',
+        product_id: '9342276534562',
     },
     {
         value: 'Thin Mint',
@@ -48,83 +116,178 @@ const flavorOptions = [
         color: '#27b376',
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Thin_Mint.png?v=1718393168',
         textColor: '#7C0101',
+        product_id: '9342359601442',
     },
 ];
 
-const CustomSelect = ({ label, onSelect, isOpen, setOpen, close, defaultText }) => {
-    const [flavors, setFlavors] = useState(flavorOptions.map(flavor => ({ ...flavor, inventory: 'Loading...' })));
-    const [selected, setSelected] = useState({ label: defaultText || 'Select flavor', color: 'white', textColor: '#7C0101' });
-    const selectRef = useRef(null);
-    const dropdownRef = useRef(null);
 
-    const handleSelect = (flavor) => {
-        setSelected(flavor);
-        setOpen(false);
-        onSelect(flavor.label);
+const CustomSelect = ({ label, onSelect, defaultText }) => {
+  const [selected, setSelected] = useState({ label: defaultText || "Select flavor", color: "white", textColor: "#7C0101" });
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [inventory, setInventory] = useState({});
+
+   useEffect(() => {
+    const fetchInventory = async () => {
+  try {
+    const response = await fetch("https://shopify-inventory-server.netlify.app/.netlify/functions/getIDS");
+    const data = await response.json();
+    console.log("Fetched Inventory Data:", data); // Debug log
+    const inventoryMap = data.reduce((acc, item) => {
+      acc[item.id] = item.inventory_quantity || "N/A";
+      return acc;
+    }, {});
+    setInventory(inventoryMap);
+  } catch (error) {
+    console.error("Error fetching inventory:", error);
+  }
+};
+
+    fetchInventory();
+  }, []);
+
+  const handleSelect = (flavor) => {
+    setSelected(flavor);
+    setIsPopupOpen(false);
+    onSelect(flavor.label);
+  };
+
+  const handleTogglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  useEffect(() => {
+    if (isPopupOpen) {
+      document.body.style.overflow = 'hidden'; // Prevent scrolling
+    } else {
+      document.body.style.overflow = ''; // Re-enable scrolling
+    }
+
+    return () => {
+      document.body.style.overflow = ''; // Cleanup
     };
+  }, [isPopupOpen]);
 
-    const handleToggle = () => {
-        setOpen(!isOpen);
-    };
+  return (
+    <div className="relative w-full select-container">
+      <label className="block mb-1 text-center">{label}</label>
+      <div
+        className="text-[.95rem] border-2 border-[#7C0101] p-2 cursor-pointer text-center font-bold h-[68px] flex items-center justify-center"
+        style={{ backgroundColor: selected.color, color: selected.textColor }}
+        onClick={handleTogglePopup}
+      >
+        {selected.label}
+      </div>
 
-    useEffect(() => {
-        const fetchInventory = async () => {
-            try {
-                const response = await fetch('/.netlify/functions/getInventory');
-                const data = await response.json();
-                const updatedFlavors = flavorOptions.map(flavor => {
-                    const inventoryData = data.find(item => item.product_id === flavor.value);
-                    return { ...flavor, inventory: inventoryData ? inventoryData.quantity : 'N/A' };
-                });
-                setFlavors(updatedFlavors);
-            } catch (error) {
-                console.error('Error fetching inventory:', error);
-            }
-        };
-
-        fetchInventory();
-    }, []); // Empty dependency array to avoid re-fetching on updates
-
-    return (
-        <div className="relative w-full select-container" ref={selectRef}>
-            <label className="block mb-1 text-center">{label}</label>
-            <div
-                className="text-[.95rem] border-2 border-[#7C0101] p-2 cursor-pointer text-center font-bold h-[68px] flex items-center justify-center"
-                style={{ backgroundColor: selected.color, color: selected.textColor }}
-                onClick={handleToggle}
+      {isPopupOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)', // Center popup
+            width: '90%',
+            maxWidth: '360px',
+            height: '60vh', // Set popup height to 60% of the visible screen
+            zIndex: 9999,
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+            backgroundColor: 'transparent',
+          }}
+        >
+          {/* Header Section */}
+          <div
+            style={{
+              width: '100%',
+              backgroundColor: '#15C5D8', // Updated background color
+              padding: '10px 0',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              position: 'relative',
+            }}
+          >
+            <h2
+              style={{
+                fontSize: '18px',
+                fontWeight: 'bold', // Bold text
+                color: '#FFFFFF', // White text
+                margin: 0,
+                textAlign: 'center',
+              }}
             >
-                {selected.label}
-            </div>
-            {isOpen && (
-                <div
-                    id="dropdown-menu"
-                    className="fixed inset-0 flex items-center justify-center z-50"
-                >
-                    <div className='bg-white border-4 border-[#7C0101] mt-1 rounded shadow-lg z-10 w-[300px]' ref={dropdownRef}>
-                        {flavors.map((flavor) => (
-                            <div
-                                key={flavor.value}
-                                className="flex items-center gap-2 px-2 py-2 cursor-pointer hover:bg-gray-200"
-                                onClick={() => handleSelect(flavor)}
-                                style={{ backgroundColor: flavor.color }}
-                            >
-                                <img src={flavor.imageUrl} alt={flavor.label} className="w-20 h-20" />
-                                <div>
-                                    <span className="font-bold text-2xl leading-tight" style={{ color: flavor.textColor }}>
-                                        {flavor.label}
-                                    </span>
-                                    <div className='text-xs' style={{ color: flavor.textColor }}>{flavor.subText}</div>
-                                    <div className="text-sm" style={{ color: flavor.textColor }}>
-                                        Inventory: {flavor.inventory}
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+              Select your flavor
+            </h2>
+            <button
+              onClick={handleClosePopup}
+              style={{
+                position: 'absolute',
+                top: '50%',
+                right: '10px',
+                transform: 'translateY(-50%)',
+                border: 'none',
+                background: 'none',
+                color: '#FFFFFF', // White close button color
+                fontSize: '20px',
+                fontWeight: 'bold',
+                cursor: 'pointer',
+              }}
+            >
+              &times;
+            </button>
+          </div>
+
+          {/* Flavor Options */}
+          <div
+            style={{
+              padding: '10px 0',
+              backgroundColor: 'transparent',
+              overflowY: 'auto',
+              flex: 1,
+            }}
+          >
+            {flavorOptions.map((flavor) => (
+              <div
+                key={flavor.value}
+                style={{
+                  backgroundColor: flavor.color,
+                  color: flavor.textColor,
+                  cursor: 'pointer',
+                  display: 'flex',
+                  alignItems: 'center',
+                  width: '100%',
+                  padding: '10px',
+                  borderRadius: '5px',
+                  marginBottom: '0', // Removed vertical spacing
+                }}
+                onClick={() => handleSelect(flavor)}
+              >
+                <img
+                  src={flavor.imageUrl}
+                  alt={flavor.label}
+                  style={{
+                    width: '75px',
+                    height: '75px',
+                    marginRight: '10px',
+                    borderRadius: '5px',
+                  }}
+                />
+                <div>
+                  <strong>{flavor.label}</strong>
+                  <p style={{ fontSize: '18px', margin: 0 }}>{flavor.subText}</p>
+                <p>Inventory: {inventory[flavor.id] || "Loading..."}</p>
                 </div>
-            )}
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CustomSelect;
