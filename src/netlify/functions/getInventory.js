11f355c012675b764d677c8893ee40e15c415e47
drinkbakesale <will@drinkbakesale.com>
2024-11-22 15:49:46 -0700

    Update getInventory.js

--- src/netlify/functions/getInventory.js
+++ src/netlify/functions/getInventory.js
@@ -1,16 +1,16 @@
-export const getInventory = async (productId) => {
+export const getInventory = async (product_Id) => {
   const { REACT_APP_SHOPIFY_STORE_URL, REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN } = process.env;
 
   if (!REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN || !REACT_APP_SHOPIFY_STORE_URL) {
     throw new Error('API Access Token or Store URL is missing');
   }
 
-  if (!productId) {
+  if (!product_Id) {
     throw new Error('Invalid product ID');
   }
 
   try {
-    const response = await fetch(`${REACT_APP_SHOPIFY_STORE_URL}/admin/api/2023-01/products/${productId}.json`, {
+    const response = await fetch(`${REACT_APP_SHOPIFY_STORE_URL}/admin/api/2023-01/products/${product_Id}.json`, {
       headers: {
         'Content-Type': 'application/json',
         'X-Shopify-Access-Token': REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN,
