96de29e4978d6288f2642701453d957ed598d89f
drinkbakesale <will@drinkbakesale.com>
2024-11-22 14:30:46 -0700

    3

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import getIDS from 'netlify/functions/getIDS'; // Import the getIDS.js function directly
+import getIDS from './netlify/functions/getIDS'; // Import the getIDS.js function directly
 
 const flavorOptions = [
         {
