b2cd22d6a5b6fc460273e2cdec613632b955b57d
drinkbakesale <will@drinkbakesale.com>
2024-11-22 17:14:28 -0700

    Update getInventory.js

--- src/netlify/functions/getInventory.js
+++ src/netlify/functions/getInventory.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  const { REACT_APP_SHOPIFY_STORE_URL, REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN } = process.env;
+  const { SHOPIFY_STORE_URL, SHOPIFY_ADMIN_API_ACCESS_TOKEN } = process.env;
 
   if (event.httpMethod === 'OPTIONS') {
     return {
@@ -17,7 +17,7 @@ exports.handler = async function (event, context) {
 
   const product_Id = event.queryStringParameters.product_Id;
 
-  if (!REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN || !REACT_APP_SHOPIFY_STORE_URL || !product_Id) {
+  if (!SHOPIFY_ADMIN_API_ACCESS_TOKEN || !SHOPIFY_STORE_URL || !product_Id) {
     return {
       statusCode: 400,
       headers: {
@@ -28,10 +28,10 @@ exports.handler = async function (event, context) {
   }
 
   try {
-    const response = await fetch(`${REACT_APP_SHOPIFY_STORE_URL}/admin/api/2023-01/products/${product_Id}.json`, {
+    const response = await fetch(`${SHOPIFY_STORE_URL}/admin/api/2023-01/products/${product_Id}.json`, {
       headers: {
         'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': REACT_APP_SHOPIFY_ADMIN_API_ACCESS_TOKEN,
+        'X-Shopify-Access-Token': SHOPIFY_ADMIN_API_ACCESS_TOKEN,
       },
     });
 
