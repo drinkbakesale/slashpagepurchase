f9a1357efc47e38f7c23fa4f9ec905110bc16b9b
drinkbakesale <will@drinkbakesale.com>
2024-11-26 09:10:32 -0700

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -22,8 +22,6 @@ const FlavorForm = () => {
   };
 
   const productNumbers = {
-    "Variety Pack #1": "48766615650594",
-    "Flavor Adventure": "48766615650594",
     "Jelly Donut": "48766619058466",
     "Thin Mint": "48766621876514",
       "Limited Edition Gingerbread": "47087547547938",
