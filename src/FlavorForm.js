3b240d95ed3da9f959bfb5552a0862e37bbf39d0
drinkbakesale <will@drinkbakesale.com>
2024-12-03 09:32:28 -0500

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -219,7 +219,7 @@ const FlavorForm = () => {
             labelTwo="You save 24%"
           >
             <p className="ml-5 mt-2">
-              Starting with this month’s flavor, Cinnamon Roll Liquor, each
+              Starting with this month’s flavor, Frosted Sugar Cookie Liquor, each
               month you receive Bakesale’s newest limited-edition flavor
               inspired by America’s favorite treats and sweets!
             </p>
