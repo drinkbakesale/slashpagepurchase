37e2c4096f5e2da0353f351bae93f8b6dc88ee15
drinkbakesale <will@drinkbakesale.com>
2024-12-05 11:13:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FlavorForm from "./FlavorForm";
 function App() {
   return (
     <div className="overflow-y-auto h-[100vh] pb-3">
-      <img src="/Purchase-order-topper-min.jpg" alt="bake-info" />
+      <img src="/sugarcookieheader.png" alt="bake-info" />
       <FlavorForm />
     </div>
   );
