3da5271baeb3b6567c78530e7f64e70c4a9991c7
drinkbakesale <will@drinkbakesale.com>
2024-12-12 14:42:11 -0700

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -26,6 +26,7 @@ const FlavorForm = () => {
     "Jelly Donut": "48766619058466",
     "Thin Mint": "48766621876514",
       "Limited Edition Gingerbread": "47087547547938",
+     "Chocolate Chip Cookie": "48766610997538",
      "Limited Edition Frosted Sugar Cookie": "49369324323106",
   };
 
