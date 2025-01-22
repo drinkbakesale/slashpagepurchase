1024dd117825c92ba98a85fd97b28a907269fcb0
drinkbakesale <will@drinkbakesale.com>
2025-01-22 10:28:28 -0700

    Update CustomSelect.js

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -1,51 +1,47 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const flavorOptions = [
-  {
-    value: "Variety Pack #1",
-    label: "Variety Pack #1",
-    subText: "Includes Cookie, Jelly Donut, Brownie, and Thin Mint liquors",
-    color: "#87c8d5",
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Flavor_Adventure_w_Shadow.png?v=1718393167",
-    textColor: "#7C0101",
-  },
-  {
-    value: "Jelly Donut",
-    label: "Jelly Donut",
-    subText: "Jam-filled joyride with bursts of raspberry jam and fluffy donut",
-    color: "#e27b9c",
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Jelly_Donut.png?v=1718393168",
-    textColor: "#7C0101",
-  },
-  {
-    value: "Chocolate Chip Cookie",
-    label: "Chocolate Chip Cookie",
-    subText: "A perfect ratio of chocolate chips to golden, buttery cookie",
-    color: "#e88b37",
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cookie_Box.png?v=1718393167",
-    textColor: "#7C0101",
-  },
-  {
-    value: "Brownie",
-    label: "Brownie",
-    subText: "Rich, fudgy, chocolatey, yet delicate, this flavor is indulgent!",
-    color: "#9965a2",
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Brownie_Box.png?v=1718393167",
-    textColor: "#FFFFFF",
-  },
-  {
-    value: "Thin Mint",
-    label: "Thin Mint",
-    subText: "Luscious chocolate cookie meets spearmint freshness",
-    color: "#27b376",
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Thin_Mint.png?v=1718393168",
-    textColor: "#7C0101",
-  },
+      {
+        value: 'Limited Edition Apple Pie',
+        label: 'Limited Edition Apple Pie',
+        subText: 'Irresistible blend of baked apple, brown sugar, and spices!',
+        color: '#A71E22',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Apple_Pie_-_Tiny-min.png?v=1737566796',
+        textColor: '#FFFFFF',
+        product_id: '9608812036386',
+    },
+ {
+        value: 'Variety Pack #1',
+        label: 'Variety Pack #1',
+        subText: 'Includes Cookie, Jelly Donut, Brownie, and Thin Mint liquors',
+        color: '#87c8d5',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Flavor_Adventure_w_Shadow.png?v=1718393167',
+        textColor: '#7C0101',
+    },
+ {
+        value: 'Jelly Donut',
+        label: 'Jelly Donut',
+        subText: 'Jam-filled joyride with bursts of raspberry jam and fluffy donut',
+        color: '#e27b9c',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Jelly_Donut.png?v=1718393168',
+        textColor: '#7C0101',
+    },
+    {
+        value: 'Chocolate Chip Cookie',
+        label: 'Chocolate Chip Cookie',
+        subText: 'A perfect ratio of chocolate chips to golden, buttery cookie',
+        color: '#e88b37',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cookie_Box.png?v=1718393167',
+        textColor: '#7C0101',
+    },
+    {
+        value: 'Thin Mint',
+        label: 'Thin Mint',
+        subText: 'Luscious chocolate cookie meets spearmint freshness',
+        color: '#27b376',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Thin_Mint.png?v=1718393168',
+        textColor: '#7C0101',
+    },
 ];
 
 const CustomSelect = ({
