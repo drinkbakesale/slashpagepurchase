c8153786433851c7d8db40c0a75ec83434d7adfa
drinkbakesale <will@drinkbakesale.com>
2025-01-22 10:17:46 -0700

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -22,11 +22,10 @@ const FlavorForm = () => {
   };
   const productNumbers = {
     "Variety Pack #1": "48766615650594",
-    "Flavor Adventure": "48766615650594",
-    "Jelly Donut": "48766619058466",
     "Chocolate Chip Cookie": "48766610997538",
-    Brownie: "48766158111010",
+    "Jelly Donut": "48766619058466",
     "Thin Mint": "48766621876514",
+      "Limited Edition Apple Pie": "49545731572002",
   };
 
   function buildShopifyCartUrl(cartItems) {
@@ -180,7 +179,7 @@ const FlavorForm = () => {
             labelTwo="You save 15%"
           >
             <p className="ml-5 mt-2">
-              Starting with this month’s flavor, Frosted Sugar Cookie Liquor, each
+              Starting with this month’s flavor, Apple Pie Liquor, each
               month you receive Bakesale’s newest limited-edition flavor
               inspired by America’s favorite treats and sweets!
             </p>
