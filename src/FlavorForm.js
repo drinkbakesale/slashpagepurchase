499afe1ae4e47759e369d98ea5975edf2656b43a
drinkbakesale <will@drinkbakesale.com>
2025-04-21 12:44:21 -0600

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -179,7 +179,7 @@ const FlavorForm = () => {
             labelTwo="You save 15%"
           >
             <p className="ml-5 mt-2">
-              Starting with this month’s flavor, Salted Caramel Chocolate Truffle Liquor, each
+              Starting with this month’s flavor, Almond Sponge Cake Liquor, each
               month you receive Bakesale’s newest limited-edition flavor
               inspired by America’s favorite treats and sweets!
             </p>
