05f93386a98f6ba31d9c140355f1bbfb1889491e
drinkbakesale <will@drinkbakesale.com>
2025-06-06 10:59:52 -0600

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -23,7 +23,6 @@ const FlavorForm = () => {
   const productNumbers = {
     "Almond Sponge Cake": "49805529383202",
     "Variety Pack #1": "48766615650594",
-    "Chocolate Chip Cookie": "48766610997538",
     "Thin Mint": "48766621876514",
   };
 
