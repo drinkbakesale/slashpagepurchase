693ab187cd83681874a0d28db8f316b86a93b678
drinkbakesale <will@drinkbakesale.com>
2025-06-06 10:59:37 -0600

    Update CustomSelect.js

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -18,15 +18,7 @@ const flavorOptions = [
         imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Flavor_Adventure_w_Shadow.png?v=1718393167',
         textColor: '#7C0101',
     },
-    {
-        value: 'Chocolate Chip Cookie',
-        label: 'Chocolate Chip Cookie',
-        subText: 'A perfect ratio of chocolate chips to golden, buttery cookie',
-        color: '#e88b37',
-        imageUrl: 'https://cdn.shopify.com/s/files/1/0677/0537/2962/files/Tiny_-_Cookie_Box.png?v=1718393167',
-        textColor: '#7C0101',
-    },
-    {
+     {
         value: 'Thin Mint',
         label: 'Thin Mint',
         subText: 'Luscious chocolate cookie meets spearmint freshness',
