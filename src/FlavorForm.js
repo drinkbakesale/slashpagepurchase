522501039b3b2f548b4f606cbef2dfd28fee3fae
drinkbakesale <will@drinkbakesale.com>
2025-06-12 15:26:39 -0600

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -22,13 +22,13 @@ const FlavorForm = () => {
   };
 
   const productNumbers = {
-    "Almond Sponge Cake": "49805529383202",
+    "Limited Edition Almond Sponge Cake": "49805529383202",
     "Variety Pack #1": "48766615650594",
     "Thin Mint": "48766621876514",
-    "Blueberry Pie": "48900876599586",
-    "5-Layer Bar": "49077761802530",
-    "Cinnamon Roll": "49275197653282",
-    "Salted Caramel Chocolate Truffle": "49729571160354"
+    "Limited Edition Blueberry Pie": "48900876599586",
+    "Limited Edition 5-Layer Bar": "49077761802530",
+    "Limited Edition Cinnamon Roll": "49275197653282",
+    "Limited Edition Salted Caramel Chocolate Truffle": "49729571160354"
   };
 
   function buildShopifyCartUrl(cartItems) {
