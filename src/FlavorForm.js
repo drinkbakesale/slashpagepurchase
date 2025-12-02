2aed9347aaac8d9a865a4164aee6d6459335f960
drinkbakesale <will@drinkbakesale.com>
2025-12-02 15:25:56 -0700

    Update FlavorForm.js

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -24,7 +24,7 @@ const FlavorForm = () => {
   const productNumbers = {
     "Limited Edition Almond Sponge Cake": "49805529383202",
     "Variety Pack #1": "48766615650594",
-    "Thin Mint": "48766621876514",
+    "Limited Edition Snickerdoodle": "49991372013858",
     "Limited Edition Blueberry Pie": "48987815674146",
     "Limited Edition Cinnamon Roll": "49275197653282",
     "Limited Edition Salted Caramel Chocolate Truffle": "49729571160354"
