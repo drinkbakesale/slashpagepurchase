7247be354c29dfadfc1e644e325325267a219e1d
drinkbakesale <will@drinkbakesale.com>
2026-03-30 15:16:11 -0600

    Remove Almond Sponge Cake and add new flavors

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -22,13 +22,9 @@ const FlavorForm = () => {
   };
 
   const productNumbers = {
-    "Almond Sponge Cake": "49805529383202",
     "Variety Pack #1": "48766615650594",
-    "Flavor Adventure": "48766615650594",
-    "Jelly Donut": "48766619058466",
-    "Chocolate Chip Cookie": "48766610997538",
-    Brownie: "48766158111010",
-    "Thin Mint": "48766621876514",
+    "Limited Edition Snickerdoodle": "49991372013858",
+    "Limited Edition Salted Caramel Chocolate Truffle": "49729571160354"
   };
 
   function buildShopifyCartUrl(cartItems) {
